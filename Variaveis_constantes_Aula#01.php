4c8be5789fc395e31d4c5f026425e73ad020731c
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-06 22:03:06 +0100

    Add files via upload

---
+++ Variaveis_constantes_Aula#01.php
@@ -0,0 +1,12 @@
+<?php
+
+        
+        
+            const nome = "Pedro Vevua";
+            const idade = 18;
+
+            echo " ".nome. " tu tens". idade ." anos de Idade.";
+
+            //NB: Para as Variaveis constantes usamos o comando Const.
+
+?>
