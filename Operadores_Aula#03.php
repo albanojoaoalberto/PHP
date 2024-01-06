8034d037771f069c87fdf45f0f4d4f9a986e7f1e
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-06 23:40:35 +0100

    Add files via upload

---
+++ Operadores_Aula#03.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+    $n1 = 14; //variavel do tipo inteiro ou Int
+    $n2 = 14; //variavel do tipo inteiro ou Int
+
+    echo "$n1 + $n2 = ".($n1+$n2) ." <br>";// Operador de Adição.
+    echo "$n1 - $n2 = ".($n1-$n2) ." <br>";// Operador de Substração.
+    echo "$n1 x $n2 = ".($n1*$n2) ." <br>";// Operador de Múltiplicação.
+    echo "$n1 ÷ $n2 = ".($n1/$n2) ." <br>";// Operador de Divisão.
+    echo "$n1 ^ $n2 = ".($n1**$n2) ." <br>";// Operador de Expoente.
+   
+    echo "E outros ....";
+   
+   
+
+    
+   
+
+
+
+?>
