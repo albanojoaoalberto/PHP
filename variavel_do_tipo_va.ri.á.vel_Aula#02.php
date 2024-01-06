0e42b8b7933c997d4cae8aad1cbe5c11ec7e8538
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-06 23:25:26 +0100

    Add files via upload

---
+++ variavel_do_tipo_va.ri.á.vel_Aula#02.php
@@ -0,0 +1,12 @@
+<?php 
+
+        $nome = "Pedro Vevua";
+        $idade =18;
+
+        $nome= "Albano";
+
+        echo "$nome tu tens ".$idade ." anos de Idade.";
+
+        //NB: Para as Variaveis que Variam usamos o comando ou o Simbolo $.
+    
+?>
