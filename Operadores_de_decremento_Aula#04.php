de58109aee14160ca7bcbd117d114151f8f7a013
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-07 22:17:29 +0100

    Add files via upload

---
+++ Operadores_de_decremento_Aula#04.php
@@ -0,0 +1,14 @@
+<?php 
+
+            $ano = Date("Y");
+            
+            
+            echo "Estamos no ano <b>".$ano . "</b> é o ano anterior foi <b>".--$ano ."</b> <br>";//decremento
+
+
+
+            
+
+
+
+?>
