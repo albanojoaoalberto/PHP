add31944b3131284449e571ce4077bb459ae7796
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-07 22:20:25 +0100

    Add files via upload

---
+++ Operadores_de_incremento_Aula#06.php
@@ -0,0 +1,14 @@
+<?php 
+
+            $ano = Date("Y");
+            
+            
+            echo "Estamos no ano <b>".$ano . "</b> é o Próximo ano será  <b>".++$ano ."</b> <br>";//incremento
+
+
+
+            
+
+
+
+?>
